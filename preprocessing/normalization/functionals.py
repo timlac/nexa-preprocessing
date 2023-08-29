8a1ea2ab1fe2ddc284c9b2201bc452f1a56521d0
timlac <tim@lachmann.nu>
2023-08-29 13:20:17 +0200

    added pandas compatibility for functionals, added tests for functionals

--- preprocessing/normalization/functionals.py
+++ preprocessing/normalization/functionals.py
@@ -1,11 +1,14 @@
+from copy import copy
+
 import numpy as np
+import pandas as pd
 
 from preprocessing.normalization.methods import select_scaler
 
 
-def within_subject_functional_normalization(x: np.ndarray,
+def within_subject_functional_normalization(x: type[np.ndarray | pd.DataFrame],
                                             subject_ids: np.ndarray,
-                                            method: str) -> np.ndarray:
+                                            method: str) -> type[np.ndarray | pd.DataFrame]:
     """
     Normalization function whereby each subject is normalized separately
 
@@ -14,6 +17,8 @@ def within_subject_functional_normalization(x: np.ndarray,
     :param method: normalization method
     :return: normalized matrix
     """
+    ret_x = copy(x)
+
     # iterate over all subject ids
     for subject_id in np.unique(subject_ids):
 
@@ -21,6 +26,16 @@ def within_subject_functional_normalization(x: np.ndarray,
         scaler = select_scaler(method)
 
         # normalize the rows corresponding for the specific subject
-        rows = np.where(subject_ids == subject_id)
-        x[rows] = scaler.fit_transform(x[rows])
-    return x
+        rows = np.where(subject_ids == subject_id)[0]
+
+        if isinstance(ret_x, pd.DataFrame):
+
+            print(ret_x.iloc[rows])
+
+            ret_x.iloc[rows] = scaler.fit_transform(ret_x.iloc[rows])
+        elif isinstance(x, np.ndarray):
+            ret_x[rows] = scaler.fit_transform(ret_x[rows])
+        else:
+            raise ValueError("input array x need to be either a pd.Dataframe or np.ndarray")
+
+    return ret_x
---
+++ tests/test_within_subject_functional_normalization.py
@@ -0,0 +1,74 @@
+import unittest
+from copy import copy
+
+import pandas as pd
+import numpy as np
+from preprocessing.normalization.functionals import within_subject_functional_normalization
+
+
+class TestLowLevelNormalization(unittest.TestCase):
+
+    def setUp(self):
+        # Setup example data for testing
+        self.x = np.array([[1, 2, 3],
+                           [4, 5, 6],
+                           [7, 8, 9],
+                           [10, 11, 12]])
+
+        self.subject_ids = np.array([1, 1, 2, 2])
+
+        self.df = pd.DataFrame(self.x, columns=['feat1', 'feat2', 'feat3'])
+
+    def test_standard_normalization(self):
+        normalized_x = within_subject_functional_normalization(self.x, self.subject_ids, "standard")
+
+        for subject_id in np.unique(self.subject_ids):
+            rows = np.where(self.subject_ids == subject_id)
+            mean = np.mean(normalized_x[rows], axis=0)
+            std = np.std(normalized_x[rows], axis=0)
+
+            # Assert that the mean is close to 0 (within a certain tolerance)
+            self.assertTrue(np.allclose(mean, 0, atol=0.01))
+
+            # Assert that the standard deviation is close to 1 (within a certain tolerance)
+            self.assertTrue(np.allclose(std, 1, atol=0.01))
+
+    def test_min_max_normalization(self):
+        normalized_x = within_subject_functional_normalization(
+            self.x, self.subject_ids, "min_max"
+        )
+        for subject_id in np.unique(self.subject_ids):
+            rows = np.where(self.subject_ids == subject_id)
+
+            # Check if all values are in the range [0, 1]
+            all_in_range = np.all((0 <= normalized_x[rows]) & (normalized_x[rows] <= 1))
+            self.assertTrue(all_in_range)
+
+    def test_standard_normalization_df(self):
+        normalized_df = within_subject_functional_normalization(self.df, self.subject_ids, "standard")
+
+        for subject_id in np.unique(self.subject_ids):
+            rows = np.where(self.subject_ids == subject_id)
+            mean = np.mean(normalized_df.iloc[rows], axis=0)
+            std = np.std(normalized_df.iloc[rows], axis=0)
+
+            # Assert that the mean is close to 0 (within a certain tolerance)
+            self.assertTrue(np.allclose(mean, 0, atol=0.01))
+
+            # Assert that the standard deviation is close to 1 (within a certain tolerance)
+            self.assertTrue(np.allclose(std, 1, atol=0.01))
+
+    def test_min_max_normalization_df(self):
+        normalized_df = within_subject_functional_normalization(
+            self.df, self.subject_ids, "min_max"
+        )
+        for subject_id in np.unique(self.subject_ids):
+            rows = np.where(self.subject_ids == subject_id)
+
+            # Check if all values are in the range [0, 1]
+            all_in_range = np.all((0 <= normalized_df.iloc[rows]) & (normalized_df.iloc[rows] <= 1))
+            self.assertTrue(all_in_range)
+
+
+if __name__ == '__main__':
+    unittest.main()
